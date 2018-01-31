b9a2a36446e671eec55e385fa8cebab1c8684e74
Jesper Carlsson <jespercarlsson@Jespers-MacBook-Air.local>
2018-01-31 14:21:46 +0100

    remade app

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -3,15 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
-
-  res.json([{
-  	"id": "1",
-  	"username": "samsepi0l"
-  }, {
-  	"id": 2,
-  	"username": "D0loresH4ze"
-  }]);
+  res.send('respond with a resource');
 });
 
 module.exports = router;
